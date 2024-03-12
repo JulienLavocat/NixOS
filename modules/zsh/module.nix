92c0d12af1a7a7d9721297abd8645d02195fc8c7
Julien Lavocat <julien.lavocat@gmail.com>
2024-03-12 09:53:00 +0100

    feat(zsh): run tmux on startup

--- modules/zsh/module.nix
+++ modules/zsh/module.nix
@@ -24,6 +24,12 @@
       if [ -z "''${WAYLAND_DISPLAY}" ] && [ "''${XDG_VTNR}" -eq 1 ]; then
         dbus-run-session Hyprland
       fi
+
+
+      if [ -z "$TMUX" ]
+      then
+        tmux attach -t main || tmux new -s main
+      fi
     '';
 
     shellAliases = {
